6c7b981164969b79088be1e55ae069908f81bc42
Luke Whyte <lukeawhyte@gmail.com>
2014-04-10 22:53:48 -0700

    fixing permalink bug

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "{title}.html"
+  blog.permalink = "/seismograph/{title}.html"
   # Matcher for blog source files
   blog.sources = "{articles}/{title}.html"
   # blog.sources = "{year}-{month}-{day}-{title}.html"

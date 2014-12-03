c7ce7bdd9185cc9771951e4bf445e39ebbafdc3d
Luke Whyte <lukeawhyte@gmail.com>
2014-12-03 10:01:25 -0800

    Fixing formatting issue

--- source/js/viz/wealth-race-gender-modules/buildWealthMap.js
+++ source/js/viz/wealth-race-gender-modules/buildWealthMap.js
@@ -45,25 +45,25 @@ define(['d3', 'underscore', 'leaflet', '../../tools/mapFactory', '../../tools/ba
 		var	renderBarChart = function (data) {
 			if (!data) return false;
 			var chart = this,
-				bars = chart.view
-					.data(data)
-					.enter()
-						.append('rect')
-						.attr({
-							width: function () {
-								return chart.w / data.length - 4;
-							},
-							y: function (d) {
-								return (chart.h - chart.scale(d)) - 20;
-							},
-							x: function (d, i) {
-								if (i === 0) return (chart.w / data.length) * i + 2;
-								else return (chart.w / data.length) * i;
-							},
-							class: function (d,i) {
-								return chart.titles[i];
-							}
-						});
+			bars = chart.view
+				.data(data)
+				.enter()
+				.append('rect')
+				.attr({
+					width: function () {
+						return chart.w / data.length - 4;
+					},
+					y: function (d) {
+						return (chart.h - chart.scale(d)) - 20;
+					},
+					x: function (d, i) {
+						if (i === 0) return (chart.w / data.length) * i + 2;
+						else return (chart.w / data.length) * i;
+					},
+					class: function (d,i) {
+						return chart.titles[i];
+					}
+				});
 
 			return setTimeout(function () {
 				bars.attr('height', function (d) {
@@ -73,135 +73,135 @@ define(['d3', 'underscore', 'leaflet', '../../tools/mapFactory', '../../tools/ba
 				chart.svg.selectAll('.wealth text')
 					.data(data)
 					.enter()
-						.append('text')
-						.text(function (d, i) {
-							if (d === null) return '';
-							else {
-								var str = d.toString();
-								return '$' + str.slice(0, -3) + ',' + str.slice(-3);
-							}
-						})
-						.attr({
-							x: function(d, i) {
-						    return i * (chart.w / data.length) + 6;
-						  },
-					  	y: function(d) {
-						    return chart.h - chart.scale(d) - 24;
-						  },
-						  "class": "wealth",
-					  	"font-size": "18px",
-					  	"fill": "#222",
-					  });
+					.append('text')
+					.text(function (d, i) {
+						if (d === null) return '';
+						else {
+							var str = d.toString();
+							return '$' + str.slice(0, -3) + ',' + str.slice(-3);
+						}
+					})
+					.attr({
+						x: function(d, i) {
+							return i * (chart.w / data.length) + 6;
+						},
+						y: function(d) {
+							return chart.h - chart.scale(d) - 24;
+						},
+						"class": "wealth",
+						"font-size": "18px",
+						"fill": "#222",
+					});
 
 				chart.svg.selectAll('.race text')
 					.data(data)
 					.enter()
-						.append('text')
-						.text(function (d, i) {
-							return chart.titles[i];
-						})
-						.attr({
-							x: function(d, i) {
-								var space = (chart.titles[i].length > 5) ? 2 : 14;
-						    return i * (chart.w / data.length) + space;
-						  },
-					  	y: function(d) {
-						    return chart.h - 2;
-						  },
-						  "class": "race",
-					  	"font-size": "18px",
-					  	"fill": "#003366"
-					  });
-			}, 100);
-		};
-
-		return d3.json('../../data/wealth-gender-race/json/county-' + dataset + '.json', function (err, dataObj) {
-			if (err) console.warn(err);
-
-			var titles = dataObj['0'];
-
-			delete dataObj['0']; // Better to remove them all together than have to skip over them when iterating
-
-			var map = getBaseMap(),
-
-				mapColor = d3.rgb(baseColor),
-
-				totalIncomeDomain = (function () {
-					var max = findExtremes(dataObj, 'max', 'incomeData.total'),
-						min = findExtremes(dataObj, 'min', 'incomeData.total'),
-						step = Math.round((max - min) / titles.length);
-					return d3.range(min, max, step);
-				}());
-
-			map.setCountyFillOpacity = buildOrdinalScale(totalIncomeDomain, d3.range(0.5,1.1,0.1));
-
-			map.setCountyColor = buildOrdinalScale(totalIncomeDomain, [mapColor.brighter(1.5), mapColor.brighter(1), mapColor.brighter(0.5), mapColor.darker(0.5), mapColor.darker(1), mapColor.darker(1.5)]);
-
-			map.bar = getBaseBarChart(map);
-
-			map.bar.scale = map.bar.setLinearScale([0, findExtremes(dataObj, 'max', 'incomeData.subPops')], [0, map.bar.h - 60]);
-
-			map.bar.titles = titles;
-
-			map.bar.view = map.bar.svg.selectAll('rect');
-
-			map.bar.renderChart = renderBarChart;
-
-			map.renderData = function () {
-				map.states.attr("d", map.path);
-				map.counties
+					.append('text')
+					.text(function (d, i) {
+						return chart.titles[i];
+					})
 					.attr({
-						d: map.path,
-						fill: function (d) {
-							return (dataObj[d.id]) ? map.setCountyColor(dataObj[d.id].incomeData.total) : 0;
+						x: function(d, i) {
+							var space = (chart.titles[i].length > 5) ? 2 : 14;
+							return i * (chart.w / data.length) + space;
 						},
-						'fill-opacity': function (d) {
-							return (dataObj[d.id]) ? map.setCountyFillOpacity(dataObj[d.id].incomeData.total) : 0;
-						}
-					})
-					.on('mouseover', function (d) {
-						var data = (dataObj[d.id]) ? dataObj[d.id].incomeData.subPops : false;
-						map.bar.write = map.bar.renderChart(data);
-					})
-					.on('mouseout', function () {
-						clearTimeout(map.bar.write);
-						map.bar.svg.selectAll('rect').transition().ease('linear').attr('height', 0).attr('y', map.bar.h).remove();
-						map.bar.svg.selectAll('text').remove();
-					})
-					.append("title")
-            .text(function (d) {
-              if (!dataObj[d.id]) return false;
-              var getPercent = function (val) {
-	              	return Math.round((val / dataObj[d.id].popData.total) * 100) + '%';
-	              },
-	              calculateOther = function () {
-	              	var totalPop = dataObj[d.id].popData.total,
-	              		currTotal = _.reduce(dataObj[d.id].popData.subPops, function (m, c, k) {
-		              		return (k !== 4) ? m + c : m;
-		              	});
-	              	return Math.round(((totalPop - currTotal) / totalPop) * 100) + '%';
-	              };
-              return dataObj[d.id].title + '\n'
-              			+ 'Population: ' + dataObj[d.id].popData.total + '\n'
-              			+ ' - Black: ' + getPercent(dataObj[d.id].popData.subPops[0])
-              			+ '\n - Asian: ' + getPercent(dataObj[d.id].popData.subPops[1])
-              			+ '\n - White: ' + getPercent(dataObj[d.id].popData.subPops[2])
-              			+ '\n - Hispanic: ' + getPercent(dataObj[d.id].popData.subPops[3])
-              			+ '\n - Other: ' + calculateOther();
-            });
-			};
-				
-			map.addUSLeafletOverlay({
-				states: 'path', // strings refer to type of svg element to be added
-				county: 'path',
-				onReady: function () { map.reset(map.renderData); } // callback for onReady
-			});
+						y: function(d) {
+							return chart.h - 2;
+						},
+						"class": "race",
+						"font-size": "18px",
+						"fill": "#003366"
+					});
+			}, 100);
+};
 
-			map.view.on('viewreset', function () {
-				map.reset(map.renderData);
-			});
+return d3.json('../../data/wealth-gender-race/json/county-' + dataset + '.json', function (err, dataObj) {
+	if (err) console.warn(err);
 
-			return map;
+	var titles = dataObj['0'];
+
+		delete dataObj['0']; // Better to remove them all together than have to skip over them when iterating
+
+		var map = getBaseMap(),
+
+		mapColor = d3.rgb(baseColor),
+
+		totalIncomeDomain = (function () {
+			var max = findExtremes(dataObj, 'max', 'incomeData.total'),
+			min = findExtremes(dataObj, 'min', 'incomeData.total'),
+			step = Math.round((max - min) / titles.length);
+			return d3.range(min, max, step);
+		}());
+
+		map.setCountyFillOpacity = buildOrdinalScale(totalIncomeDomain, d3.range(0.5,1.1,0.1));
+
+		map.setCountyColor = buildOrdinalScale(totalIncomeDomain, [mapColor.brighter(1.5), mapColor.brighter(1), mapColor.brighter(0.5), mapColor.darker(0.5), mapColor.darker(1), mapColor.darker(1.5)]);
+
+		map.bar = getBaseBarChart(map);
+
+		map.bar.scale = map.bar.setLinearScale([0, findExtremes(dataObj, 'max', 'incomeData.subPops')], [0, map.bar.h - 60]);
+
+		map.bar.titles = titles;
+
+		map.bar.view = map.bar.svg.selectAll('rect');
+
+		map.bar.renderChart = renderBarChart;
+
+		map.renderData = function () {
+			map.states.attr("d", map.path);
+			map.counties
+			.attr({
+				d: map.path,
+				fill: function (d) {
+					return (dataObj[d.id]) ? map.setCountyColor(dataObj[d.id].incomeData.total) : 0;
+				},
+				'fill-opacity': function (d) {
+					return (dataObj[d.id]) ? map.setCountyFillOpacity(dataObj[d.id].incomeData.total) : 0;
+				}
+			})
+			.on('mouseover', function (d) {
+				var data = (dataObj[d.id]) ? dataObj[d.id].incomeData.subPops : false;
+				map.bar.write = map.bar.renderChart(data);
+			})
+			.on('mouseout', function () {
+				clearTimeout(map.bar.write);
+				map.bar.svg.selectAll('rect').transition().ease('linear').attr('height', 0).attr('y', map.bar.h).remove();
+				map.bar.svg.selectAll('text').remove();
+			})
+			.append("title")
+			.text(function (d) {
+				if (!dataObj[d.id]) return false;
+				var getPercent = function (val) {
+					return Math.round((val / dataObj[d.id].popData.total) * 100) + '%';
+				},
+				calculateOther = function () {
+					var totalPop = dataObj[d.id].popData.total,
+					currTotal = _.reduce(dataObj[d.id].popData.subPops, function (m, c, k) {
+						return (k !== 4) ? m + c : m;
+					});
+					return Math.round(((totalPop - currTotal) / totalPop) * 100) + '%';
+				};
+				return dataObj[d.id].title + '\n'
+					+ 'Population: ' + dataObj[d.id].popData.total + '\n'
+					+ ' - Black: ' + getPercent(dataObj[d.id].popData.subPops[0])
+					+ '\n - Asian: ' + getPercent(dataObj[d.id].popData.subPops[1])
+					+ '\n - White: ' + getPercent(dataObj[d.id].popData.subPops[2])
+					+ '\n - Hispanic: ' + getPercent(dataObj[d.id].popData.subPops[3])
+					+ '\n - Other: ' + calculateOther();
+			});
+		};
+		
+		map.addUSLeafletOverlay({
+			states: 'path', // strings refer to type of svg element to be added
+			county: 'path',
+			onReady: function () { map.reset(map.renderData); } // callback for onReady
 		});
-	};
+
+		map.view.on('viewreset', function () {
+			map.reset(map.renderData);
+		});
+
+		return map;
+	});
+};
 });
